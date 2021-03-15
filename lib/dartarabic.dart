da3805b181d074d36351a075259e47db8702c3fa
thexaib <thexeb@gmail.com>
2021-03-15 18:14:00 +0500

    making methods static

--- lib/dartarabic.dart
+++ lib/dartarabic.dart
@@ -1,12 +1,9 @@
 library dartarabic;
 
-import 'package:dartarabic/constants.dart';
 import 'package:dartarabic/operations.dart';
 
-/// A Calculator.
-class DartArabic {
-  /// Returns [value] plus 1.
-  String removeHarakat(String text) => ArOp.strip_harakat(text);
-  String removeTashkeel(String text) => ArOp.strip_tashkeel(text);
-  String removeDiacritics(String text) => ArOp.strip_diacritics(text);
+abstract class DartArabic {
+  static String removeHarakat(String text) => ArOp.strip_harakat(text);
+  static String removeTashkeel(String text) => ArOp.strip_tashkeel(text);
+  static String removeDiacritics(String text) => ArOp.strip_diacritics(text);
 }
--- test/dartarabic_test.dart
+++ test/dartarabic_test.dart
@@ -3,25 +3,22 @@ import 'package:dartarabic/dartarabic.dart';
 
 void main() {
   test('Remove Harakat', () {
-    final parser = DartArabic();
     String test = "الْعَرَبِيَّةُ";
     String fixture = "العربيّة";
-    expect(parser.removeHarakat(test), fixture);
-    expect(parser.removeHarakat(test), isNot(equals(test)));
+    expect(DartArabic.removeHarakat(test), fixture);
+    expect(DartArabic.removeHarakat(test), isNot(equals(test)));
   });
   test('Remove Tashkeel', () {
-    final parser = DartArabic();
     String test = "الْعَرَبِيَّةُ";
     String fixture = "العربية";
-    expect(parser.removeTashkeel(test), fixture);
-    expect(parser.removeTashkeel(test), isNot(equals(test)));
+    expect(DartArabic.removeTashkeel(test), fixture);
+    expect(DartArabic.removeTashkeel(test), isNot(equals(test)));
   });
   test('Remove Diacritic', () {
-    final parser = DartArabic();
     String test = "الْعَرَبِيَّةُ";
     String fixture = "العربية";
-    expect(parser.removeDiacritics(test), fixture);
-    expect(parser.removeDiacritics(test), isNot(equals(test)));
+    expect(DartArabic.removeDiacritics(test), fixture);
+    expect(DartArabic.removeDiacritics(test), isNot(equals(test)));
   });
 
 
